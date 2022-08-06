d9537f372b0583f54ea513739be7412017d09170
salmankhan12345522 <110522928+salmankhan12345522@users.noreply.github.com>
2022-08-06 19:03:45 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['91**********']
-global.premium = ['91**********']
-global.ownernomer = '91**********'
-global.ownername = '𝑺𝑶𝑼𝑳 𝑫𝑶𝑴𝑰𝑵𝑨𝑻𝑶𝑹'
-global.botname = '𝑫𝑶𝑴𝑰𝑵𝑨𝑻𝑶𝑻 𝑺𝑬𝑹 𝑩𝑶𝑻'
+global.owner = ['923231582002']
+global.premium = ['923231582002']
+global.ownernomer = '923231582002'
+global.ownername = '๛ジW6丨SALMANツ'
+global.botname = '๛ジW6丨SALMANツ 𝑩𝑶𝑻'
 global.footer = '©sᴇʀᴅᴏᴍɪɴᴀᴛᴏʀ'
 global.ig = 'https://github.com/dominator454'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/dominator454/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCuYd8PWkU5QDCExj-ku8bGQ'
-global.packname = '𝑰𝑻𝒁 𝑴𝑬 𝑺𝑶𝑼𝑳 𝑫𝑶𝑴𝑰𝑵𝑨𝑻𝑶𝑻'
-global.author = '𝑰𝑻𝒁 𝑴𝑬 𝑺𝑶𝑼𝑳 𝑫𝑶𝑴𝑰𝑵𝑨𝑻𝑶𝑹'
+global.packname = '𝑰𝑻s 𝑴𝑬 ๛ジW6丨SALMANツ'
+global.author = '𝑰𝑻s ๛ジW6丨SALMANツ'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
